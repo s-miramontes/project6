306bb944e022b6b22f17e22a59fce7b4c2ce6948
s-miramontes <silvsilva@berkeley.edu>
2022-02-22 20:20:13 -0800

    code clean up

--- regression/logreg.py
+++ regression/logreg.py
@@ -5,14 +5,17 @@ import matplotlib.pyplot as plt
 
 class BaseRegressor():
     def __init__(self, num_feats, learning_rate=0.1, tol=0.001, max_iter=100, batch_size=12):
+
         # initializing parameters
         self.W = np.random.randn(num_feats + 1).flatten()
+
         # assigning hyperparameters
         self.lr = learning_rate
         self.tol = tol
         self.max_iter = max_iter
         self.batch_size = batch_size
         self.num_feats = num_feats
+
         # defining list for storing loss history
         self.loss_history_train = []
         self.loss_history_val = []
@@ -27,16 +30,22 @@ class BaseRegressor():
         pass
     
     def train_model(self, X_train, y_train, X_val, y_val):
+
         # Padding data with vector of ones for bias term
         X_train = np.hstack([X_train, np.ones((X_train.shape[0], 1))])
         X_val = np.hstack([X_val, np.ones((X_val.shape[0], 1))])
+
+        
         # Defining intitial values for while loop
         prev_update_size = 1
         iteration = 1
+
         # Gradient descent
         while prev_update_size > self.tol and iteration < self.max_iter:
+
             # Shuffling the training data for each epoch of training
             shuffle_arr = np.concatenate([X_train, np.expand_dims(y_train, 1)], axis=1)
+
             # In place shuffle
             np.random.shuffle(shuffle_arr)
             X_train = shuffle_arr[:, :-1]
@@ -44,30 +53,42 @@ class BaseRegressor():
             num_batches = int(X_train.shape[0]/self.batch_size) + 1
             X_batch = np.array_split(X_train, num_batches)
             y_batch = np.array_split(y_train, num_batches)
+
             # Generating list to save the param updates per batch
             update_size_epoch = []
-            # Iterating through batches (full for loop is one epoch of training)
+
+            # Iterating through batches (full for loop is one epoch of training), training denoted by batches
             for X_train, y_train in zip(X_batch, y_batch):
+
                 # Making prediction on batch
-                y_pred = self.make_prediction(X_train)
+                y_pred = self.make_prediction(X_train) # your TODO
+
                 # Calculating loss
-                loss_train = self.loss_function(X_train, y_train)
+                loss_train = self.loss_function(X_train, y_train) # your TODO
+
                 # Adding current loss to loss history record
                 self.loss_history_train.append(loss_train)
+
                 # Storing previous weights and bias
                 prev_W = self.W
+
                 # Calculating gradient of loss function with respect to each parameter
-                grad = self.calculate_gradient(X_train, y_train)
+                grad = self.calculate_gradient(X_train, y_train) #your TODO
+
                 # Updating parameters
                 new_W = prev_W - self.lr * grad 
                 self.W = new_W
+
                 # Saving step size
                 update_size_epoch.append(np.abs(new_W - prev_W))
+
                 # Validation pass
                 loss_val = self.loss_function(X_val, y_val)
                 self.loss_history_val.append(loss_val)
+
             # Defining step size as the average over the past epoch
             prev_update_size = np.mean(np.array(update_size_epoch))
+
             # Updating iteration number
             iteration += 1
     
@@ -78,12 +99,16 @@ class BaseRegressor():
         loss_hist = self.loss_history_train
         loss_hist_val = self.loss_history_val
         assert len(loss_hist) > 0, "Need to run training before plotting loss history"
+
         fig, axs = plt.subplots(2, figsize=(8,8))
         fig.suptitle('Loss History')
+
         axs[0].plot(np.arange(len(loss_hist)), loss_hist)
         axs[0].set_title('Training Loss')
+
         axs[1].plot(np.arange(len(loss_hist_val)), loss_hist_val)
         axs[1].set_title('Validation Loss')
+        
         plt.xlabel('Steps')
         axs[0].set_ylabel('Train Loss')
         axs[1].set_ylabel('Val Loss')
