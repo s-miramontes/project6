70b459d2ff842675a3e371ac4f8765837221461b
s-miramontes <silvsilva@berkeley.edu>
2022-02-22 20:13:55 -0800

    init

--- regression/__init__.py
+++ regression/__init__.py
@@ -2,3 +2,8 @@
 BMI203: Biocomputing algorithms Winter 2022
 Assignment 6: Logistic regression
 """
+
+from .logrec import LogisticRegression
+from .utils import loadDataset
+
+__version__ = '0.0.1'
